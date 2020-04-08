7cc44dd65e062b2dfea505eddba27eb3dce90d65
D.J. Sutherland <djs@djsutherland.ml>
2020-04-08 03:50:17 -0500

    start, partially working

---
+++ server.js
@@ -0,0 +1,113 @@
+const express = require('express');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const minimist = require('minimist');
+const WebSocket = require('ws');
+
+let args = minimist(process.argv, {
+  'default': {port: 5000, socket_port: 5001},
+  'alias': {p: 'port'}
+});
+
+const app = express();
+app.set('view engine', 'pug');
+app.use(express.urlencoded({extended: false}));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Dicer: Dice for DIE'
+  });
+});
+
+app.post('/login/', (req, res) => {
+  var nick = req.body.nickname;
+  res.redirect(`/roll/${nick}/`);
+});
+
+app.get('/roll/:nickname/', (req, res) => {
+  res.render('roll', {
+    title: 'Dicer: Dice for DIE',
+    nickname: req.params.nickname
+  });
+});
+
+
+var webserver;
+if (args.ssl_key) {
+  webserver = https.createServer({
+    key: fs.readFileSync(args.ssl_key),
+    cert: fs.readFileSync(args.ssl_cert)
+  }, app);
+  if (!args.port) { args.port = 443; }
+} else {
+  webserver = http.createServer(app);
+  if (!args.port) { args.port = 80; }
+}
+
+const socketserver = new WebSocket.Server({server: webserver});
+
+function roll(sides) { return Math.floor(Math.random() * sides) + 1; }
+const sidesByKind = {
+  dictator: 4,
+  d6: 6,
+  bad: 6,
+  fool: 6,
+  knight: 8,
+  neo: 10,
+  godbinder: 12,
+  master: 20
+};
+
+socketserver.on('connection', ws => {
+  ws.on('message', data => {
+    data = JSON.parse(data);
+    switch (data.action) {
+      case "hello":
+        ws.nickname = data.nickname;
+        break;
+
+      case "roll":
+        let dice = data.dice || [];
+        let sides = dice.map(d => sidesByKind[d]);
+        let rolls = sides.map(roll);
+        var response = JSON.stringify({
+          action: "results",
+          nickname: ws.nickname,
+          dice: dice,
+          rolls: rolls,
+          sides: sides
+        });
+        socketserver.clients.forEach(client => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(response);
+          }
+        });
+        break;
+
+      default:
+        console.log(`Unknown requested action - ${data}`);
+    }
+  });
+  ws.on('close', e => {
+    console.log(`${ws.nickname || "[unknown]"} disconnected`);
+  });
+  ws.on('error', e => {
+    switch (e.code) {
+      case "ECONNRESET":
+        break;
+      default:
+        console.log(`client ${ws.nickname || "[unknown]"} error: ${e}`);
+        break;
+    }
+  });
+});
+
+webserver.listen(args.port, () => {
+ console.log(`Express running → PORT ${webserver.address().port}`);
+});
+
+// webserver.listen(args.socket_port);
+// console.log(`Socket server running → PORT ${args.socket_port}`);
---
+++ src/rolls.js
@@ -0,0 +1,83 @@
+const api = (() => {
+  var ws, nickname;
+
+  function setup() {
+    nickname = document.getElementById('welcome').dataset.nickname;
+
+    var proto = `ws${window.location.protocol == "https:" ? "s" : ""}`;
+    ws = new WebSocket(`${proto}://${window.location.host}`);
+    ws.onmessage = event => {
+      let data = JSON.parse(event.data);
+      switch (data.action) {
+        case "results":
+          showResults(data);
+          break;
+        default:
+          console.error(`Unknown action: ${data}`);
+          break;
+      }
+
+    };
+    ws.onopen = event => {
+      ws.send(JSON.stringify({action: "hello", nickname: nickname}));
+    };
+
+    for (let img of document.querySelectorAll("#dice img")) {
+      img.addEventListener("click", toggleSelected(img));
+    }
+    document.getElementById("roll").addEventListener("click", roll);
+  }
+
+  function roll() {
+    let selected = document.querySelectorAll("#dice img.selected");
+
+    ws.send(JSON.stringify({
+      action: "roll",
+      dice: Array.from(selected).map(e => e.dataset.kind)
+    }));
+    
+    selected.forEach(img => img.classList.remove("selected"));
+  }
+
+  function showResults(response) {
+    let node = document.createElement("div");
+    node.classList.add("result");
+    node.innerHTML = `
+      <div class="name">${response.nickname}</div>
+      <div class="dice"></div>
+    `;
+    var dicenode = node.querySelector('.dice');
+    for (let i = 0; i < response.dice.length; i++) {
+      let child = document.createElement('span');
+      child.dataset.kind = response.dice[i];
+      child.innerHTML = `d${response.sides[i]}: <b>${response.rolls[i]}</b>`;
+      dicenode.appendChild(child);
+    }
+
+    let rolls = document.getElementById('rolls');
+    rolls.insertBefore(node, rolls.firstChild);
+  }
+
+  function toggleSelected(img) {
+    return event => {
+      if (img.classList.contains("selected")) {
+        img.classList.remove("selected");
+      } else {
+        img.classList.add("selected");
+      }
+    };
+  }
+
+  return {
+    setup: setup
+  };
+})();
+
+function ready(fn) {
+  if (document.readyState != 'loading') {
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+ready(api.setup);
